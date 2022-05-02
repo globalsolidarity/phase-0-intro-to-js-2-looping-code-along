78d50bff7c8c1b4acad6c38871d85a6c755b12d0
Kaitlin O'Connor <globalsolidaritytiktok@gmail.com>
2022-05-01 20:55:08 -0600

    commit tree 60e1709d09b796c3ee3b5b8466f641c45a0597b1

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Code your solutions in this file
+function writeCards(name, event) {
+let people = []
+for (let i = 0; i < name.length; i++) {
+     people.push(`Thank you, ${name[i]}, for the wonderful ${event} gift!`)
+}
+return people
+}
+
+function countDown(i) {
+while (i >= 0) {
+    console.log(i);
+    i -=1;
+  }
+}
